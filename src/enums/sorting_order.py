5a790c3cfe678cba97b199a10ee5d4168c25d528
Renato Moura Martins Medeiros <renamedrj@gmail.com>
2023-01-22 01:34:29 -0300

    Additional parameters to bubblesort

--- src/enums/sorting_order.py
+++ src/enums/sorting_order.py
@@ -5,6 +5,6 @@ SortingOrder = Enum('SortingOrder', ['Ascending', 'Descending'])
 
 def is_ordered(sorting_order):
   if sorting_order == SortingOrder.Ascending:
-    return lambda x : x < 0
+    return lambda x : x <= 0
   elif sorting_order == SortingOrder.Descending:
-    return lambda x : x > 0
+    return lambda x : x >= 0
--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -3,12 +3,15 @@ from src.enums.sorting_order import SortingOrder, is_ordered
 
 def bubble_sort(elems : list[any], sorting_order = SortingOrder.Ascending, comparison = lambda a, b : a - b) -> list[any]:
   result = elems.copy()
-  ordered = is_ordered(sorting_order)  
-  for i in range(len(result)):
-    for j in range(i, len(result)):
-      comp_result = comparison(result[i], result[j])
+  ordered = is_ordered(sorting_order)
+  
+  swapped = True
+  while swapped:
+    swapped = False
+    for i in range(1, len(result)):    
+      comp_result = comparison(result[i - 1], result[i])
       if not ordered(comp_result):
-        result[i], result[j] = result[j], result[i]
+        result[i], result[i - 1] = result[i - 1], result[i]
+        swapped = True
 
   return result
-
